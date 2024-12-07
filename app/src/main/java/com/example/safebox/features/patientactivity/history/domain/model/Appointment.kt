21249c7e680c620b2569279aee36d8683fa00b6c
Leo <leonardonifinluri@gmail.com>
2024-12-07 09:02:16 +0800

    feat: add appointment dataclass

---
+++ app/src/main/java/com/example/safebox/features/patientactivity/history/domain/model/Appointment.kt
@@ -0,0 +1,7 @@
+package com.example.safebox.features.patientactivity.history.domain.model
+
+data class Appointment(
+    var psychologistId: String = "",
+    var psychologistName: String = "",
+    var day: Int = 0,
+)
